f7dc8863da52e610d48e0870e9e015fb9585ec74
MohsenArvan <f.n.raiat@gmail.com>
2024-05-13 17:25:10 +0330

    Create Migrate table Task

---
+++ database/migrations/2024_05_13_133221_create_tasks_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('description');
+            $table->string('lable');
+            $table->string('status');
+            $table->foreignId('board_id')->constrained();
+            $table->dateTime('due_date')->nullable();
+            $table->dateTime('completed_at')->nullable();
+            $table->string('priority')->default('low');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tasks');
+    }
+};
