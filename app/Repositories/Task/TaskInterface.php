098c1eb53dcabe92fa258e70750835c8b59c67e5
MohsenArvan <f.n.raiat@gmail.com>
2024-05-17 19:06:15 +0330

    refactor: Update TaskInterface to include CRUD methods for tasks

--- app/Repositories/Task/TaskInterface.php
+++ app/Repositories/Task/TaskInterface.php
@@ -1,10 +1,18 @@
 <?php 
 
-namespace App\Repository\Task;
+namespace App\Repositories\Task;
 
 use App\Repositories\RepositoryInterface;
 
 interface TaskInterface extends RepositoryInterface
 {
-    //
+    public function getAllTask($board_id);
+
+    public function find($task);
+
+    public function create($request = []);
+
+    public function update($id, $request = []);
+
+    public function delete($task_id);
 }
