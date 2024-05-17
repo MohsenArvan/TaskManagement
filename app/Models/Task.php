121973b38718ddadccfbbe5a059dc50fb9e38bff
MohsenArvan <f.n.raiat@gmail.com>
2024-05-17 19:05:09 +0330

    refactor: Add tasks relationship to Board model and fillable properties to Task model

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -8,4 +8,20 @@ use Illuminate\Database\Eloquent\Model;
 class Task extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'description',
+        'lable',
+        'status',
+        'board_id',
+        'due_date',
+        'completed_at',
+        'priority',
+    ];
+
+    public function board()
+    {
+        return $this->belongsTo(Board::class);
+    }
 }
