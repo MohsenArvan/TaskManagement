05559f1ec74b67807f03ba2bff04c6a284342004
MohsenArvan <f.n.raiat@gmail.com>
2024-05-17 19:06:41 +0330

    refactor: Add getAllTask, storeTask, showTask, updateTask, and destroy methods to TaskServiceInterface

--- app/Services/Task/TaskServiceInterface.php
+++ app/Services/Task/TaskServiceInterface.php
@@ -4,5 +4,13 @@ namespace App\Services\Task;
 
 interface TaskServiceInterface
 {
-    public function index();
+    public function getAllTask($board_id);
+
+    public function storeTask($data, $board_id);
+
+    public function showTask($task);
+
+    public function updateTask($data, $task_id);
+
+    public function destroy($task_id);
 }
--- app/Services/Task/TaskServices.php
+++ app/Services/Task/TaskServices.php
@@ -2,11 +2,45 @@
 
 namespace App\Services\Task;
 
+use App\Repositories\Task\TaskRepository;
+use App\Repositories\Task\TaskInterface;
+
 class TaskServices implements TaskServiceInterface
 {
-    public function index()
+    public function getAllTask($board_id)
     {
-        return 'asdfadf';
+        return app(TaskInterface::class)->getAllTask($board_id);
+    }
+
+    public function storeTask($data, $board_id)
+    {
+        $data['board_id'] = $board_id;
+        return app(TaskInterface::class)->create($data);
+    }
+
+    public function showTask($task)
+    {
+        return app(TaskInterface::class)->find($task);
+    }
+
+    public function updateTask($data, $task_id)
+    {
+        return app(TaskInterface::class)->update($task_id, $data);
+    }
+
+    public function destroy($task_id)
+    {
+        return app(TaskInterface::class)->delete($task_id);
+    }
+
+    public function search($keyword)
+    {
+        // return ;
+    }
+
+    public function filter($filter)
+    {
+        // return ;
     }
 
 }
